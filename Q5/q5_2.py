8dc122b0584b321fca0b0815df5dda6493f9b212
dang2 <dndeo10814@gmail.com>
2023-01-19 05:48:02 +0900

    Q5 solved

--- Q5/q5_2.py
+++ Q5/q5_2.py
@@ -4,18 +4,23 @@
 #10000까지 진행 후 출력하면
 #나머지 값들은 모두 셀프넘버일 것
 
-ans = list(map(str,range(1, 10001)))
-result = []
-num1 = 0
-for n in ans :
-    num = list(''.join(n))
-    num_n = 0
-    for i in range(len(num)) :
-        num_n = num_n + int(num[i])
-    num1 = num_n +int(n)
-    if f'{num1}' in ans :
-        ans.remove(f'{num1}')
-    else :
-        pass
+def d(n) :
+    n = str(n)
+    num_str = list(''.join(n))
+    result = 0
+    num_int = 0
+    for i in range(len(num_str)) :
+        num_int = num_int + int(num_str[i])
+    result = num_int +int(n)
+    return result
 
-print(ans)
+ans_except = []
+
+for i in range(30000) :
+    if d(i) <= 10000 :
+        ans_except.append(d(i))
+for j in range(10001) :
+    if j not in ans_except :
+        print(j)
+
+
