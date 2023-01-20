6ce948c6bfbed37c7e072a99ec88f857492a16ae
dang2 <dndeo10814@gmail.com>
2023-01-20 10:57:12 +0900

    seat move

--- Q5/q5_3.py
+++ Q5/q5_3.py
@@ -1,26 +1,32 @@
-#자리수와 그 다음 자리수의 차 == 그 다음 자리수와 그 다다음 자리수의 차
-#-> 자리수와 그 다다음 자리수의 합 == 그 다음 자리수 * 2
-#이면 등차수열
+
 #N 보다 작은 한수의 갯수?
 #1~N 까지 반복
 #등차수열인지 확인하는 함수를 만들고
-#for i in range(1,N+1)
-n = input()
+#for i in range(1,N+1)의 i 에 대해
+#-> 자리수와 그 다다음 자리수의 합 == 그 다음 자리수 * 2
+
+n = int(input())
 count = 0
 def han(n) :
+    result = 0
     num_list = []
     while n != 0 :
         num_list.insert(0, n % 10)
         n = n // 10
-    for i in range(len(n)-2) :
-        if num_list[i] + num_list[i+2] != num_list[i+1] :
-            return False
-        
-    
-    
-    return num_list
+    for i in range(len(num_list)-2):
+        if len(num_list) == 1 or len(num_list) ==2 :
+            result = 1
+            return result
+        elif num_list[i] + num_list[i+2] != 2*num_list[i+1] :
+            result = 0
+            return result
+    else :
+        result = 1
+        return result
+for i in range(1,n+1) :
+    count += han(i)
 
-print(han(123456))
+print(count)
 
 
 
