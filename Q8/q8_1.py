7005a1972bcf85450a56cb4a7c401a3434ae3fbe
dang2 <dndeo10814@gmail.com>
2023-01-27 00:25:12 +0900

    q8-1

--- Q8/q8_1.py
+++ Q8/q8_1.py
@@ -1,9 +1,12 @@
-num = int(input())
-numbers = list(map(int,input().split()))
+#숫자의 약수가 1과 자기자신 밖에 없을 때
+t = int(input())
+num = list(map(int,input().split()))
 
-for j in numbers:
-    for i in range(1,1000+1):
-        if j % i == 0:
-            num = num - 1
-print(num)
-
+for i in num:
+    for j in range(1,i+1):
+        if i == 1:
+            t -=1
+        elif i % j == 0 and i != j and j != 1:
+            t -= 1
+            break
+print(t)
