eb8c5d87ed4415c2fcce541d839a857bc7bbc6a1
dang2 <dndeo10814@gmail.com>
2023-02-06 18:00:39 +0900

    0206

--- SWEA/sw_6019_fly/fly.py
+++ SWEA/sw_6019_fly/fly.py
@@ -1,9 +1,11 @@
-import sys
-sys.stdin = open('s_input.txt')
+# import sys
+# sys.stdin = open('s_input.txt')
 
-t = int(input())
+# t = int(input())
 
-for tc in range(1, t+1):
-    D, A, B, F = map(int,input().split())
-    rlt = (D / (A + B)) * F
-    print(f'#{tc} {rlt}')
+# for tc in range(1, t+1):
+#     D, A, B, F = map(int,input().split())
+#     rlt = (D / (A + B)) * F
+#     print(f'#{tc} {rlt}')
+
+print(3 and 5)
