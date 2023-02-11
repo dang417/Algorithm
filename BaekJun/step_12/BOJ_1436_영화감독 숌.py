9a8575808479278c371f08fe32d066e343bc22ac
dang2 <dndeo10814@gmail.com>
2023-02-11 13:17:51 +0900

    step 12 13 14

---
+++ BaekJun/step_12/BOJ_1436_영화감독 숌.py
@@ -0,0 +1,7 @@
+# 666이 들어간 수를 작은 순서대로 정렬...
+end_of_world_list = []
+n = int(input())
+for i in range(666, 10000000):
+    if '666' in str(i):
+        end_of_world_list.append(i)
+print(end_of_world_list[n-1])
