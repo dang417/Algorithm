ae7d55a546081cae154f5f8589d5e258d855d443
dang2 <dndeo10814@gmail.com>
2023-03-09 00:45:46 +0900

    0309

--- BaekJun/step_17/BOJ_1904_01타일.py
+++ BaekJun/step_17/BOJ_1904_01타일.py
@@ -1,10 +1,5 @@
 import sys
 sys.stdin = open('input.txt')
 
-def w(a, b, c):
-
-
 n = int(input())
-
-char = ['00'] * (n//2) + ['1'] * n
 
--- BaekJun/step_17/BOJ_9184_신나는 함수 실행.py
+++ BaekJun/step_17/BOJ_9184_신나는 함수 실행.py
@@ -4,16 +4,20 @@ sys.stdin = open('input.txt')
 def w(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         return 1
-    elif a < b and b < c:
-        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-    else:
-        return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a < b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return dp[a][b][c]
 
-while True:
-    a, b, c =map(int, input().split())
-    if (a, b, c) == (-1, -1, -1):
+dp = [[[0]*21for _ in range(21)] for _ in range(21)]
+
+while 1:
+    a, b, c = map(int, input().split())
+    if a == -1 and b == -1 and c == -1:
         break
-    elif a > 20 and b > 20 and c > 20:
-        print(w(20, 20, 20))
-    else:
-        print(w(a, b, c))
+    print(f'w({a}, {b}, {c}) =', w(a, b, c))
